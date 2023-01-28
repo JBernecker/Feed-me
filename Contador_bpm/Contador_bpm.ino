9e95fdb921f8c1747782393564adfc743ddbb72b
Cinthia Cavalheiro <Cinthia>
2023-01-28 10:27:00 -0300

    Primeira versão

---
+++ Contador_bpm/Contador_bpm.ino
@@ -0,0 +1,40 @@
+#include "BluetoothSerial.h"
+
+int sensorPort = 4;
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+} 
+
+long timer = millis();
+int count = 0;
+bool ultimoSinal = false;
+
+void loop() {
+  
+  if( millis() - timer > 4000 ){
+    SerialBT.printf("%d bpm\r\n", count * 15);
+    count = 0;
+    timer = millis();
+  }else{
+    int value = analogRead(sensorPort);
+    if(ultimoSinal == false){
+      if( value > 2200 ){
+        ultimoSinal = true;
+        count++;
+      }
+    }else{
+      if( value < 2200 ){
+        ultimoSinal = false;
+      }
+    }
+    
+    Serial.println(value);
+  }
+   
+  delay(20);
+}
